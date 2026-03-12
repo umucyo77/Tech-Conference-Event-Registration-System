60a191ded8483a420c5ac2c8390383465048c2d1
umucyo77 <eumucyuvakumana77@gmail.com>
2026-03-12 09:39:04 +0200

    Registration Confirmation Page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Information from './components/Information.jsx'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import RegistrationForm from './components/RegistrationForm.jsx'
 import RegistrationReview from './components/RegistrationReview.jsx'
+import Confirmation from './components/Confirmation.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,8 +20,8 @@ function App() {
           <Route path='/' element={<Information />}> </Route>
           <Route path='/register' element={<RegistrationForm />}>Registration FOrm</Route>
           <Route path='/review' element={<RegistrationReview />}></Route>
-           {/*<Route path='/confirmation' element={<Confirmation />}></Route>
-          <Route path="/my-registration" element={<MyRegistration />} ></Route> */}
+           <Route path='/confirmation' element={<Confirmation />}></Route>
+          {/*<Route path="/my-registration" element={<MyRegistration />} ></Route> */}
         </Routes>
       </BrowserRouter>   
 
---
+++ src/components/Confirmation.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Confirmation() {
+
+  const data = JSON.parse(localStorage.getItem("registrationData"));
+
+  localStorage.setItem("savedRegistration", JSON.stringify(data));
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-50 p-4">
+      <div className="max-w-md w-full text-center bg-white rounded-xl shadow-2xl p-8">
+        <h1 className="text-3xl font-bold mb-4 text-green-600">
+          Registration Successful!
+        </h1>
+
+        <p className="mb-6 text-gray-700">Your seat has been reserved.</p>
+
+        <Link to="/confirmation">
+          <button className="px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition shadow">
+            View My Registration
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Confirmation;
